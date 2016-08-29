b0c55f6eaf3df6ca663b5e28ba0056492819d98f
New Song <New Song@DESKTOP-47VBUR9>
2016-08-29 23:21:24 +0800

    设计模式

---
+++ DesignPattern/src/strategySample2/MallardDuck.java
@@ -0,0 +1,17 @@
+package strategySample2;
+/**
+ * ҰѼ
+ * @author New Song
+ *
+ */
+public class MallardDuck extends Duck{
+
+	public MallardDuck(FlyBehaviour flyBehaviour, QuackBehaviour quackBehaviour) {
+		super(flyBehaviour, quackBehaviour);
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("һֻҰѼ~~~");
+	}
+}
